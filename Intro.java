435ff37af22bf4525983188c767c8eb460030bd1
Debpratim <debpratimsharma33@gmail.com>
2025-02-21 19:34:09 +0530

    initial commit

---
+++ Intro.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Intro 
+{
+    public static void main(String[] args ){
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = in.nextLine();
+        System.out.println("My name is "+name);
+        in.close();
+    }
+}
